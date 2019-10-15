3f31d5b183b739e426a6d4fb625110c5927d6c34
Michael Hrivnak <mhrivnak@gmail.com>
2019-10-15 06:55:06 -0400

    updated

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+var contentArea = document.getElementById("content");
+var numQuestions = questions.length;
+
+document.getElementById("getStarted").addEventListener("click",function(){
+    contentArea.innerHTML = "Ya'll gone";
+    startTimer();
+});
+
+function startTimer(){
+    
+}
